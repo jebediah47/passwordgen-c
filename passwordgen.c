159241a7534d07787c8137ea635825d1cf5d1edf
jebediah47 <jebediah47@protonmail.com>
2022-07-03 21:53:52 +0300

    Formatted passwordgen.c

--- passwordgen.c
+++ passwordgen.c
@@ -2,23 +2,25 @@
  * File: paswordgen.c
  * License: GNU Affero General Public License v3.0
  * Author: jebediah47
- * For more licensing information visit: https://www.gnu.org/licenses/agpl-3.0.en.html
+ * For more licensing information visit:
+ * https://www.gnu.org/licenses/agpl-3.0.en.html
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char alphanum[] =
+    "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main(int argc, char *argv[]) {
-	int n;
-	n = atoi(argv[1]);
+  int n;
+  n = atoi(argv[1]);
 
-	srand(time(NULL));
-	for(int i = 0; i < n; i++)
-		printf("%c", alphanum[rand() % sizeof(alphanum)-1]);
-		printf("\n");
+  srand(time(NULL));
+  for (int i = 0; i < n; i++)
+    printf("%c", alphanum[rand() % sizeof(alphanum) - 1]);
+  printf("\n");
 
-	return 0;
+  return 0;
 }
